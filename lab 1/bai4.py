88c35d1e0c5ff4f3dc0aee02ba2ce5d03c734dda
22_Trương Thị Thu Hoài_0054 <ttthoai.dhkl17a2hn@sv.uneti.vn>
2024-03-14 23:46:48 +0700

    Create bai4.py

---
+++ lab 1/bai4.py
@@ -0,0 +1,7 @@
+a,h = map(float,input("nhập các số đo: ").split(","))
+Sxq = 2*a*h
+Stp = Sxq + a**2
+V = (1/3)*a**2*h
+print("diện tích xung quanh hình chóp tứ giác đều:",Sxq)
+print("diện tích toàn phần  hình chóp tứ giác đều:",Stp)
+print("thể tích hình chóp tứ giác đều:",V)
