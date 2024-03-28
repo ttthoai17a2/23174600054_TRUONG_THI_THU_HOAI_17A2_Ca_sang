6c24806775e040b0493a3f0567e8b22f23bdffb8
22_Trương Thị Thu Hoài_0054 <ttthoai.dhkl17a2hn@sv.uneti.vn>
2024-03-29 00:10:16 +0700

    Create bai2.py

---
+++ lab 3/bai2.py
@@ -0,0 +1,17 @@
+# a
+'''
+tong = 0
+for i in range(2000, 4001):
+        if i % 7 == 0 and i % 5 != 0:
+            tong += i
+            ket_qua = tong
+print(f"Tổng  chia hết cho 7 nhưng không chia hết cho 5 từ 2000 đến 4000 là: {ket_qua}")
+'''
+#b
+tong = 0
+for i in range(500, 1001):
+        if i % 4 == 0 and i % 6 != 0:
+            tong += i
+            ket_qua = tong
+print(f"Tổng  chia hết cho 4 nhưng không chia hết cho 6 từ 500 đến 1000 là: {ket_qua}")
+
