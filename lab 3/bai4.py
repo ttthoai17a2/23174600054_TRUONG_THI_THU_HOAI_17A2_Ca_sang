8b6fe3db2f874bf67c00fe3048bf10dc266ec462
22_Trương Thị Thu Hoài_0054 <ttthoai.dhkl17a2hn@sv.uneti.vn>
2024-03-29 10:09:45 +0700

    Create bai4.py

---
+++ lab 3/bai4.py
@@ -0,0 +1,14 @@
+# HÌNH1
+for i in range(5):
+    print(" " * (5 - i - 1) + "* " * (i + 1))
+
+for i in range(4):
+    print(" " * (i + 1) + "* " * (4 - i))
+'''
+#HÌNH2
+for i in range(7):
+    print(" " * (7 - i - 1) + "*" * (2 * i + 1))
+
+for _ in range(3):
+    print(" " * 5 + "***")
+'''
