5c1e5feea5e08af1be2ac35703e4f905fde56b5d
22_Trương Thị Thu Hoài_0054 <ttthoai.dhkl17a2hn@sv.uneti.vn>
2024-04-05 00:41:57 +0700

    Create bai1b.py

---
+++ lab 4/bai1b.py
@@ -0,0 +1,10 @@
+numbers = []
+tong =0
+
+while tong <= 1000:
+    n = int(input("Nhập một số nguyên dương: "))
+    if n >0:
+        tong += n
+        if n % 2 == 0:
+            numbers.append(n)
+print("Các số lẻ đã nhập",numbers)
